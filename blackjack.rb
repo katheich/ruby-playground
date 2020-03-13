0b30593f1a39cb4dc4aece6696c6beddcaab29db
Kathrin Eichinger <kathrin801@gmail.com>
2020-03-13 15:45:32 +0000

    opening hand

--- blackjack.rb
+++ blackjack.rb
@@ -2,10 +2,6 @@
 # create deck of cards
 deck = []
 
-suits = ['hearts', 'diamonds', 'spades', 'clubs']
-
-card_values = [ ['Q', 10], ['J', 10], ['K', 10], ['A', 11] ]
-
 class Card
   def initialize(suit, value, points)
     @suit = suit
@@ -25,9 +21,14 @@ class Card
     @points
   end
 
-
+  def to_s
+    puts "#{suit}, #{value} (#{points} points)"
+  end
 
 end
+
+suits = ['hearts', 'diamonds', 'spades', 'clubs']
+card_values = [ ['Q', 10], ['J', 10], ['K', 10], ['A', 11] ]
 
 for i in 2..10
   card_values.push([i.to_s, i])
@@ -40,3 +41,34 @@ suits.each do |suit|
   end
 end
 
+# shuffle deck
+deck.shuffle!
+
+
+# calculate score of a hand
+def calcScore(hand)
+  score = hand.inject(0) { |sum, n| sum + n.points }
+  return score
+end
+
+
+# opening hand
+sam = []
+dealer = []
+
+sam.push(*deck.slice!(0, 2))
+dealer.push(*deck.slice!(0, 2))
+
+# puts 'Deck'
+# deck.each { |card| card.to_s }
+puts "Sam: #{calcScore(sam)}"
+sam.each { |card| card.to_s }
+
+puts "Dealer: #{calcScore(dealer)}"
+dealer.each { |card| card.to_s }
+
+
+# prompt user to hit or stand
+
+
+# if user not lost, dealer plays until >= 17
